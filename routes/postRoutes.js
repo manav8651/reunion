57a7bd32a8a0dedc14d062707154d2e0100c772a
Manav Raj <mr.manav.raj.2805@gmail.com>
2022-05-21 10:56:33 +0530

    Added files

---
+++ routes/postRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const postController = require('../controller/postController');
+const authController = require('../controller/authController');
+
+const router = express.Router();
+router.get('/:id', postController.getPost);
+//router.get('/allposts',authController.protect, postController.getAllPostByUser);
+
+router.post('/', authController.protect, postController.createNewPost);
+router.delete('/:id', authController.protect, postController.deletePost);
+
+router.patch('/like/:id', authController.protect, postController.likePost);
+router.patch('/unlike/:id', authController.protect, postController.unlikePost);
+router.patch('/comment/:id', authController.protect, postController.addcomment);
+
+
+
+
+module.exports = router;
